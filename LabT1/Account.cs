2a1cdc290c58894b8e218359ac0cf9c890258e12
Andekl <andreas.eklund94@gmail.com>
2018-06-07 23:50:35 +0200

    deposit is done

--- LabT1/Account.cs
+++ LabT1/Account.cs
@@ -10,7 +10,7 @@ namespace LabT1
         public double Balance
         {
             get { return initialBalance; }
-            private set { initialBalance += value; }
+            private set { initialBalance = value; }
         }
 
         public Account(double initialBalance)
@@ -20,22 +20,38 @@ namespace LabT1
 
         public void Deposit(double amount)
         {
-            try
+            if (double.IsNaN(amount))
             {
-                Balance = amount;
+                throw new Exception("Can't deposit a value that isnt a number");
             }
-            catch (OverflowException)
+            else if (double.IsInfinity(amount) || double.IsNegativeInfinity(amount) || double.IsPositiveInfinity(amount))
             {
-                Console.WriteLine("You got too much money, open a new account!");
+                throw new Exception("Wow you got alot of money! here's my paypal:");
             }
+            else if (amount <= 0)
+            {
+                throw new Exception("Must deposit a value that is greater than 0");
+            }
+            else Balance += amount;
             
-            if(amount > 1)
+        }
+
+        public void Withdraw(double amount)
+        {
+            if(Balance >= amount)
             {
-                Console.WriteLine("You can't deposit less than 1kr, try again");
+                Balance -= amount;
             }
-            
 
         }
+
+        public bool Transfer(Account target, double amount)
+        {
+            Balance -= amount;
+            target.Balance += amount;
+            return true;
+        }
+
 
     }
 }
--- LabT1Test/UnitTest1.cs
+++ LabT1Test/UnitTest1.cs
@@ -6,31 +6,95 @@ namespace LabT1Test
 {
     public class UnitTest1
     {
+        Account account = new Account(10);
         [Fact]
         public void ShouldSetBalance()
         {
-            Account account1 = new Account(1000);
-            Assert.Equal(1000, account1.Balance);
+            Account account = new Account(1000);
+            Assert.Equal(1000, account.Balance);
         }
 
 
         [Fact]//Deposit
         public void ShouldAddAmountToBalance()
-        {
-            Account account = new Account(10);
+        {            
             account.Deposit(2);
             double expected = 12;
             double actual = account.Balance;
             Assert.Equal(expected, actual);
         }
 
-        [Fact]
-        public void ShoulReturnErrorMessageWhenDepositingUnderOneKr()
+        [Fact]//Deposit
+        public void ShouldHandleDepositIfAmountIsNan()
         {
-            Account account = new Account(10); 
-            account.Deposit(0);
-            string expected = Console.WriteLine("You can't deposit less than 1kr, try again");
+            Assert.Throws<Exception>(() =>
+            {
+                account.Deposit(double.NaN);
+            });
         }
+
+        [Fact]//Deposit
+        public void ShouldHandleDepositIfAmountIsPositiveInfinity()
+        {
+            Assert.Throws<Exception>(() =>
+            {
+                account.Deposit(double.PositiveInfinity);
+            });
+        }
+
+        [Fact]//Deposit
+        public void ShouldHandleDepositIfAmountIsNegativeInfinity()
+        {
+            Assert.Throws<Exception>(() =>
+            {
+                account.Deposit(double.NegativeInfinity);
+            });
+        }
+
+        [Fact]//Deposit
+        public void ShouldHandeDepositIfAmountIsZero()
+        {
+            Assert.Throws<Exception>(() =>
+            {
+                account.Deposit(-1);
+            });
+        }
+
+        [Fact]//Deposit
+        public void ShouldHandeDepositIfAmountIsLessThanZero()
+        {
+            Assert.Throws<Exception>(() =>
+            {
+                account.Deposit(0);
+            });
+        }        
+
+
+        [Fact]//Withdraw
+        public void ShouldWithdrawAmountFromBalance()
+        {
+            account.Withdraw(2);
+            double exptected = 8;
+            double actual = account.Balance;
+            Assert.Equal(exptected, actual);
+        }
+
+        [Fact]//Transfer
+        public void TransferAmountFromAccountTooAccountB()
+        {
+            Account account = new Account(100);
+            Account accountB = new Account(20);
+            account.Transfer(accountB, 50);
+            
+            double expectedAccountBalanceAfterTransfer = 50;
+            double actualAccountBalanceAfterTransfer = account.Balance;
+            Assert.Equal(expectedAccountBalanceAfterTransfer, actualAccountBalanceAfterTransfer);
+
+            double exptectedAccountB_Balance = 70;
+            double actualAccountB_Balance = accountB.Balance;
+            Assert.Equal(exptectedAccountB_Balance, actualAccountB_Balance);
+        }
+      
 
 
     }
